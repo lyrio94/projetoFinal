b247d1f565d8a974ada5fbbd9cbfa1296321bf3a
lyrio94 <alexandrelyrio94@hotmail.com>
2021-01-08 02:50:01 -0300

    first commit

---
+++ app/src/main/java/projeto/iesb/br/projetofinal/view/activity/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package projeto.iesb.br.projetofinal.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import projeto.iesb.br.projetofinal.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
